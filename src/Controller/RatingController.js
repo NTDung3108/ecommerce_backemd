58a85f112bbb6da1e0d1ad6c936bb511d1d6cad1
dungnt <dungnt11@sunshinegroup.vn>
2022-06-17 17:23:01 +0700

    commit

--- src/Controller/RatingController.js
+++ src/Controller/RatingController.js
@@ -74,16 +74,14 @@ const addNewRating = async(req, res = response) => {
             });
         }
 
-        const respone = await pool.query(`CALL SP_NEW_COMMENT (?,?,?,?,?)`, in_productId, in_personId, in_rating, in_comment, '', in_date);
-
-        console.log(respone);
+        await pool.query(`CALL SP_NEW_COMMENT (?,?,?,?,?);`, [in_productId, in_personId, in_rating, in_comment, in_date]);
 
         return res.json({
             resp : true,
             msj : 'Add new rating success',
         });
     } catch (error) {
-        return res.json({
+        return res.status(400).json({
             resp : false,
             msj : error,
         });
@@ -91,25 +89,6 @@ const addNewRating = async(req, res = response) => {
     
 }
 
-// DELIMITER //
-// CREATE PROCEDURE SP_GET_RATTING (IN in_idProduct INT)
-// BEGIN
-// 	SELECT AVG(rating) as rating FROM rating WHERE product_id = in_idProduct;
-// END//
-
-// DELIMITER //
-// CREATE PROCEDURE SP_GET_COMMENT (IN in_idProduct INT)
-// BEGIN
-// 	SELECT r.person_id, p.firstName, p.lastName, p.image, r.comment, r.rating, r.date FROM rating AS r
-//     INNER JOIN person AS p ON p.uid = r.person_id
-//     WHERE product_id = in_idProduct;
-// END//
-
-// DELIMITER //
-// CREATE PROCEDURE SP_NEW_COMMENT (IN in_idProduct INT, IN in_personId VARCHAR(50), IN in_rating double, IN in_date DATE)
-// BEGIN
-// 	INSERT INTO rating(product_id, person_id, rating, comment, date) VALUE (in_idProduct, in_personId, in_rating, in_date);
-// END//
 
 module.exports = {
     getCommet,
